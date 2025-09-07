377c24c34c0a4fda5d255d40d3a53ea9123b4842
Rliop913 <87229271+Rliop913@users.noreply.github.com>
2025-09-08 03:59:44 +0900

    added examples

---
+++ addons/Project_DJ_Godot/examples/config_and_play.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03bbcdbede41cb23150a4cf3d557e9efcf351e9fee6ee80dfeaddd8678191362
+size 1211
---
+++ addons/Project_DJ_Godot/examples/config_and_play_with_realtime_FX.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:790a6aaa4857cdf48e12a771640f69e9dda12bb81b5b5ccba4f68d2c0c58367b
+size 1979
