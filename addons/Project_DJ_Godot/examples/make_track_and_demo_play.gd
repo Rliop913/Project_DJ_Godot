11826e1d750a6b19e3fa436629f2af07f0176655
Rliop913 <87229271+Rliop913@users.noreply.github.com>
2025-09-08 21:15:31 +0900

    added more examples

--- addons/Project_DJ_Godot/examples/make_track_and_demo_play.gd
+++ addons/Project_DJ_Godot/examples/make_track_and_demo_play.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac22bc5a6357022bf907f750c14115708fb8aba04cc7b9405af1cbe2fdb5cd46
-size 2636
+oid sha256:ae9ab59a767d02f6a37705f849c567708acf9e068dd994719f7a82f6e0b826d8
+size 1579
